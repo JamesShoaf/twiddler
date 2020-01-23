f1bdf727ffce4cabdb015e6fd33bb72442d3f546
James <IEatPie@github.com>
2020-01-23 15:59:06 -0800

    implement user tweet history and add logo feature in css

--- get_tweets.js
+++ get_tweets.js
@@ -13,9 +13,21 @@
 // }
 
 var index = 0;
+var currentUser = 'home';
+var $tweetCorral = $('<div id="corral"></div>');
+
+var clear_the_corral = function(){
+  index = 0;
+  var $tweetCorral = $('<div id="corral"></div>');
+  $("#corral").remove();
+  $tweetCorral.appendTo($('body'));
+}
 
 var get_new_tweets = function(){
-  
+  if(currentUser !== 'home'){
+    clear_the_corral();
+    currentUser = 'home';
+  }
   while (index < streams.home.length){
     get_tweet(streams.home[index]);
     index += 1;
@@ -29,8 +41,18 @@ var get_tweet = function(tweet){
   var $post = $(`<div class="post">${tweet.message}</div>`);
   var $date = $(`<span class="date">${tweet.created_at}</span>`);
   var $handle = $(`<span class="handle">@${tweet.user}</span>`);
+  $handle.click(function(){get_tweets_by_user(tweet.user);});
   $tweet.append($handle);
   $tweet.append($date);
   $tweet.append($post);
   $tweet.prependTo("#corral");
+}
+
+var get_tweets_by_user = function(user){
+  clear_the_corral();
+  while (index < streams.users[user].length){
+    get_tweet(streams.users[user][index]);
+    index++;
+  }
+  currentUser = user;
 }
